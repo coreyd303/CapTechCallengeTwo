f942c0f8e852549af617ddf53da41acdc79638a8
Corey Davis <cdavis@captechconsulting.com>
2017-06-29 16:53:55 -0600

    [WIP] Implement HTTPS call to USGS

--- CapTechChallengeTwo/CapTechChallengeTwo/ViewController.swift
+++ CapTechChallengeTwo/CapTechChallengeTwo/ViewController.swift
@@ -7,17 +7,44 @@
 //
 
 import UIKit
+import os.log
 
 class ViewController: UIViewController {
 
+  let appDelegate = (UIApplication.shared.delegate) as! AppDelegate
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    requestEarthquakeData()
+
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+
+  enum JSONError: String, Error {
+    case NoData = "Error: No Data"
+    case ConversionFailed = "Error: Conversion from JSON failed"
+  }
+
+  func requestEarthquakeData() {
+    if let url = URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson") {
+      let request = NSMutableURLRequest(url: url)
+
+      URLSession.shared.dataTask(with: request as URLRequest) {
+        (data, response, error) in
+          if error != nil {
+            os_log("request failed to complete", type: .error)
+            print(error as Any)
+          } else  {
+            let json = try? JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary
+            print(json)
+          }
+
+        }.resume()
+    }
   }
 
 
